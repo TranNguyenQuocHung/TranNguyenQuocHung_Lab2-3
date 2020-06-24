4eeba8e2bc1bdc4b674b591ec64a5d332748d030
Asus <Asus@DESKTOP-Q5N94MJ>
2020-06-24 21:16:22 +0700

    Add project files.

---
+++ QuanLiSach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuanLiSach.MvcApplication" Language="C#" %>
---
+++ QuanLiSach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuanLiSach.Startup))]
+namespace QuanLiSach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
